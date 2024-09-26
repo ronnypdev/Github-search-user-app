69616a714f3086981784aa76e549284cff4c47bc
Ronny Perez <ronnypdev@gmail.com>
2024-09-26 14:43:57 -0400

    Fix Api Connection

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,31 @@
 import { useState, useEffect } from "react"
 import Button from "./base/Button"
 import SearchIcon from "../assets/icon-search.svg"
-import ProfileAvatar from "../assets/images/Oval.png"
 import LocationIcon from "../assets/icon-location.svg"
 import WebsiteIcon from "../assets/icon-website.svg"
 import TwitterIcon from "../assets/icon-twitter.svg"
 import CompanyIcon from "../assets/icon-company.svg"
 
-type userInfo = {
-  avatar_url: string,
-  name: string,
-  bio: string,
-  created_at: string,
-  public_repos: number,
-  followers: number,
-  following: number,
-  location: string,
-  twitter_username: null | string,
-  blog: string,
-  company: string,
+interface userInfo {
+  avatar_url: string;
+  name: string;
+  bio: string;
+  created_at: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  location: string;
+  twitter_username: null | string;
+  blog: string;
+  company: string;
+  login: string;
 }
 
 export default function Form() {
-  const [userData, setUserData] = useState<object | null>({});
+  const [userData, setUserData] = useState<userInfo | null>(null);
   const [userName, setUserName] = useState<string>("ronnypdev");
+  const [loading, setLoading] = useState<boolean>(true);
+  
 
   useEffect(() => {
     async function fetchUserData() {
@@ -34,8 +36,8 @@ export default function Form() {
           throw new Error(`HTTP error: ${response.status}`)
         }
 
-        const data: userInfo = await response.json();
-        setUserData(data);
+        const result: userInfo = await response.json();
+        setUserData(result);
 
       } catch (error) {
         console.log(`Could not find users with that name please try again ${error}`)
@@ -59,52 +61,54 @@ export default function Form() {
       </label>
     </form>
 
-    <div className="results bg-white grid grid-cols-3 gap-[2px] p-9 shadow-paleWhite">
-      <div className="col-start-1 col-end-2 flex justify-center items-start">
-        <img className="w-[117px] h-[117px] rounded-full" src={userData.avatar_url} alt="Profile Avatar Image" />
-      </div>
+    {userData &&
+      <div className="results bg-white grid grid-cols-3 gap-[2px] p-9 shadow-paleWhite">
+        <div className="col-start-1 col-end-2 flex justify-center items-start">
+          <img className="w-[117px] h-[117px] rounded-full" src={userData.avatar_url} alt="Profile Avatar Image" />
+        </div>
 
-      <div className="profile-info col-start-2 col-span-3">
-        <header className="flex justify-between items-center">
-          <h1 className="heading-1">{userData.login}</h1>
-          <p className="paragraph-text">Joined 25 Jan 2011</p>
-        </header>
-        <h3 className="heading-3 mb-5">@octocat</h3>
-        <p className="paragraph-text mb-8">This profile has no bio</p>
-        <div className="stats-section">
-          <div>
-            <h4 className="heading-4">Repos</h4>
-            <h2 className="heading-2">8</h2>
+        <div className="profile-info col-start-2 col-span-3">
+          <header className="flex justify-between items-center">
+            <h1 className="heading-1">{userData.login}</h1>
+            <p className="paragraph-text">Joined 25 Jan 2011</p>
+          </header>
+          <h3 className="heading-3 mb-5">@{userData.login}</h3>
+          <p className="paragraph-text mb-8">This profile has no bio</p>
+          <div className="stats-section">
+            <div>
+              <h4 className="heading-4">Repos</h4>
+              <h2 className="heading-2">8</h2>
+            </div>
+            <div>
+              <h4 className="heading-4">Followers</h4>
+              <h2 className="heading-2">3938</h2>
+            </div>
+            <div>
+              <h4 className="heading-4">Following</h4>
+              <h2 className="heading-2">9</h2>
+            </div>
           </div>
-          <div>
-            <h4 className="heading-4">Followers</h4>
-            <h2 className="heading-2">3938</h2>
-          </div>
-          <div>
-            <h4 className="heading-4">Following</h4>
-            <h2 className="heading-2">9</h2>
-          </div>
-        </div>
-        <div className="links">
-          <div className="link-item mb-4">
-            <img className="w-4 h-5 shrink-0" src={LocationIcon} alt="Google Map Icon" />
-            <a className="paragraph-text ml-3" href="#">San Francisco</a>
-          </div>
-          <div className="link-item mb-4">
-            <img className="w-4 h-5 shrink-0" src={TwitterIcon} alt="Twitter Icon" />
-            <a className="paragraph-text ml-3" href="#">Not Available</a>
-          </div>
-          <div className="link-item mb-4">
-            <img className="w-4 h-5 shrink-0" src={WebsiteIcon} alt="Website Icon" />
-            <a className="paragraph-text ml-3" href="#">https://github.blog</a>
-          </div>
-          <div className="link-item mb-4">
-            <img className="w-4 h-5 shrink-0" src={CompanyIcon} alt="Company Icon" />
-            <a className="paragraph-text ml-3" href="#">@github</a>
+          <div className="links">
+            <div className="link-item mb-4">
+              <img className="w-4 h-5 shrink-0" src={LocationIcon} alt="Google Map Icon" />
+              <a className="paragraph-text ml-3" href="#">San Francisco</a>
+            </div>
+            <div className="link-item mb-4">
+              <img className="w-4 h-5 shrink-0" src={TwitterIcon} alt="Twitter Icon" />
+              <a className="paragraph-text ml-3" href="#">Not Available</a>
+            </div>
+            <div className="link-item mb-4">
+              <img className="w-4 h-5 shrink-0" src={WebsiteIcon} alt="Website Icon" />
+              <a className="paragraph-text ml-3" href="#">https://github.blog</a>
+            </div>
+            <div className="link-item mb-4">
+              <img className="w-4 h-5 shrink-0" src={CompanyIcon} alt="Company Icon" />
+              <a className="paragraph-text ml-3" href="#">@github</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    }
 
     </>
   )
