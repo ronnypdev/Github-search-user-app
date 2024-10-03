5ae289bdc95bdb99d13421ea410dae2d3a5935f8
Ronny Perez <ronnypdev@gmail.com>
2024-10-03 18:17:02 -0400

    Fixed indentation on header block

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,21 +13,21 @@ export default function Header({
   return (
     <>
       <header className="flex justify-between items-center my-9">
-      <h1 className={`${toggleDarkMode ? "heading-1-white" : "heading-1"}`}>devfinder</h1>
-      <div className="flex justify-center items-center gap-4 cursor-pointer" onClick={toggleOnClick}>
-        {toggleDarkMode ? (
-          <>
-            <h4 className="heading-4-white uppercase font-bold tracking-[2.5px] hover:text-white">light</h4>
-            <SunIcon fillColor="#FFFFFF" />
-          </>
-        ) : (
-          <>
-            <h4 className="heading-4 uppercase font-bold tracking-[2.5px] hover:text-black">dark</h4>
-            <MoonIcon fillColor="#697C9A" />
-          </>
-        )}
-      </div>
-    </header>
+        <h1 className={`${toggleDarkMode ? "heading-1-white" : "heading-1"}`}>devfinder</h1>
+        <div className="flex justify-center items-center gap-4 cursor-pointer" onClick={toggleOnClick}>
+          {toggleDarkMode ? (
+            <>
+              <h4 className="heading-4-white uppercase font-bold tracking-[2.5px] hover:text-white">light</h4>
+              <SunIcon fillColor="#FFFFFF" />
+            </>
+          ) : (
+            <>
+              <h4 className="heading-4 uppercase font-bold tracking-[2.5px] hover:text-black">dark</h4>
+              <MoonIcon fillColor="#697C9A" />
+            </>
+          )}
+        </div>
+      </header>
     </>
   )
 }
